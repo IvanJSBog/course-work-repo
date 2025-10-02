9266e263626f6158a0d09f643db5423ee94a019b
IvanJSBog <ivanpuskin6139@gmail.com>
2025-10-02 05:55:07 +0300

    updating main.ts with cookieParser, validationPipe and cors for using frontend in future

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ConfigService} from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser(configService.getOrThrow<string>('COOKIE_SECRET')));
+
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+
+  app.enableCors({
+    origin: configService.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  await app.listen(configService.getOrThrow('APPLICATION_PORT') ?? 3000);
 }
 bootstrap();
